fb2ce8b92c07797b8b62d67d219513a39b6b07b5
Jesus Rosas <jesusr93@outlook.es>
2017-05-18 17:59:02 -0500

    init files

---
+++ app.js
@@ -0,0 +1,77 @@
+
+//closure
+// function patito() {
+
+//   var nombre = "Mimi"; // variable definida
+//   console.log("Nada");
+  
+//   // Funcion definida
+//   function getNombre() {
+//     console.log(nombre);
+//   }
+//   getNombre();
+// }
+
+
+// let btn = document.getElementById('btn');
+
+// btn.addEventListener('click', (e) => {
+//   console.log(e.srcElement.hidden = true);
+// })
+
+// window.addEventListener('load', function() {
+//   let ref = patito();
+// })
+
+
+// window.addEventListener('load', () => {
+
+//   let container = document.createElement("div");
+//   let text = document.createTextNode(" Este es el contenido ")
+//   container.appendChild(text)
+//   document.body.appendChild(container);
+
+// });
+
+
+// createElement()
+
+// appendChild()
+
+// removeChild()
+
+// parentNode()
+
+// replaceChild()
+
+// Node List
+
+// getElementsByTagName()
+// Como es una lista de elementos se pueden aplicar estructuras de control como
+// ciclos
+// Un nodeList es un Array de elementos
+
+
+// window.addEventListener('load', () => {
+//   var seccion = document.createElement("section");
+//   var parrafo = document.createElement("p")
+//   document.body.appendChild(seccion);
+//   let flag = true;
+  
+//   if(flag) {
+//     document.body.replaceChild(parrafo, seccion);
+//   } else {
+//     var text = document.createTextNode("Hola amigosss");
+//     seccion.appendChild(text);
+//   }
+
+
+//   let li = document.getElementsByTagName('li');
+//   console.log( li instanceof HTMLCollection );
+
+//   for(let i = 0; i <= li.length -1; i++) {
+        
+//   }
+
+// });
+
